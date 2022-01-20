55ad87ee6ce3d9f95fc079a21b3e823b3736fd11
nujabness <m.elassouri@gmail.com>
2022-01-20 16:44:21 +0100

    Init

--- src/main/java/com/pi4j/example/MinimalExample.java
+++ src/main/java/com/pi4j/example/MinimalExample.java
@@ -1,165 +1,66 @@
 package com.pi4j.example;
 
-/*-
- * #%L
- * **********************************************************************
- * ORGANIZATION  :  Pi4J
- * PROJECT       :  Pi4J :: EXAMPLE  :: Sample Code
- * FILENAME      :  MinimalExample.java
- *
- * This file is part of the Pi4J project. More information about
- * this project can be found here:  https://pi4j.com/
- * **********************************************************************
- * %%
- * Copyright (C) 2012 - 2021 Pi4J
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import com.pi4j.Pi4J;
-import com.pi4j.context.Context;
-import com.pi4j.io.gpio.analog.AnalogInput;
-import com.pi4j.io.gpio.digital.DigitalInput;
-import com.pi4j.io.gpio.digital.DigitalOutput;
-import com.pi4j.io.gpio.digital.DigitalState;
-import com.pi4j.io.gpio.digital.PullResistance;
-import com.pi4j.platform.Platforms;
 import com.pi4j.util.Console;
 
 /**
- * <p>This example fully describes the base usage of Pi4J by providing extensive comments in each step.</p>
+ * <p>AnalogInputExample class.</p>
  *
- * @author Frank Delporte (<a href="https://www.webtechie.be">https://www.webtechie.be</a>)
+ * @author Robert Savage (<a href="http://www.savagehomeautomation.com">http://www.savagehomeautomation.com</a>)
  * @version $Id: $Id
  */
 public class MinimalExample {
 
-    private static final int PIN_MOISTURE_SENSOR = 4;
+    /** Constant <code>ANALOG_INPUT_PIN=4</code> */
+    public static final int ANALOG_INPUT_PIN = 4;
 
     /**
-     * This application blinks a led and counts the number the button is pressed. The blink speed increases with each
-     * button press, and after 5 presses the application finishes.
+     * <p>Constructor for AnalogInputExample.</p>
+     */
+    public MinimalExample() {
+    }
+
+    /**
+     * <p>main.</p>
      *
      * @param args an array of {@link java.lang.String} objects.
      * @throws java.lang.Exception if any.
      */
     public static void main(String[] args) throws Exception {
-        // Create Pi4J console wrapper/helper
+
+        // create Pi4J console wrapper/helper
         // (This is a utility class to abstract some of the boilerplate stdin/stdout code)
         final var console = new Console();
 
-        // Print program title/header
-        console.title("<-- The Pi4J Project -->", "Minimal Example project");
+        // print program title/header
+        console.title("<-- The Pi4J Project -->", "Basic Analog Input Example");
 
-        // ************************************************************
-        //
-        // WELCOME TO Pi4J:
-        //
-        // Here we will use this getting started example to
-        // demonstrate the basic fundamentals of the Pi4J library.
-        //
-        // This example is to introduce you to the boilerplate
-        // logic and concepts required for all applications using
-        // the Pi4J library.  This example will do use some basic I/O.
-        // Check the pi4j-examples project to learn about all the I/O
-        // functions of Pi4J.
-        //
-        // ************************************************************
+        // allow for user to exit program using CTRL-C
+        console.promptForExit();
 
-        // ------------------------------------------------------------
-        // Initialize the Pi4J Runtime Context
-        // ------------------------------------------------------------
-        // Before you can use Pi4J you must initialize a new runtime
-        // context.
-        //
-        // The 'Pi4J' static class includes a few helper context
-        // creators for the most common use cases.  The 'newAutoContext()'
-        // method will automatically load all available Pi4J
-        // extensions found in the application's classpath which
-        // may include 'Platforms' and 'I/O Providers'
+        // Initialize Pi4J with an auto context
+        // An auto context includes AUTO-DETECT BINDINGS enabled
+        // which will load all detected Pi4J extension libraries
+        // (Platforms and Providers) in the class path
         var pi4j = Pi4J.newAutoContext();
 
-        // ------------------------------------------------------------
-        // Output Pi4J Context information
-        // ------------------------------------------------------------
-        // The created Pi4J Context initializes platforms, providers
-        // and the I/O registry. To help you to better understand this
-        // approach, we print out the info of these. This can be removed
-        // from your own application.
-        // OPTIONAL
-        PrintInfo.printLoadedPlatforms(console, pi4j);
-        PrintInfo.printDefaultPlatform(console, pi4j);
-        PrintInfo.printProviders(console, pi4j);
+        // create an analog input instance using the default analog input provider
+        var input = pi4j.analogInput().create(ANALOG_INPUT_PIN);
 
-        // Here we will create I/O interfaces for a (GPIO) digital output
-        // and input pin. We define the 'provider' to use PiGpio to control
-        // the GPIO.
-//        var ledConfig = DigitalOutput.newConfigBuilder(pi4j)
-//                .id("led")
-//                .name("LED Flasher")
-//                .address(PIN_LED)
-//                .shutdown(DigitalState.LOW)
-//                .initial(DigitalState.LOW)
-//                .provider("pigpio-digital-output");
-//        var led = pi4j.create(ledConfig);
-//
-//        var buttonConfig = DigitalInput.newConfigBuilder(pi4j)
-//                .id("button")
-//                .name("Press button")
-//                .address(PIN_BUTTON)
-//                .pull(PullResistance.PULL_DOWN)
-//                .debounce(3000L)
-//                .provider("pigpio-digital-input");
-//        var button = pi4j.create(buttonConfig);
-//        button.addListener(e -> {
-//            if (e.state() == DigitalState.LOW) {
-//                pressCount++;
-//                console.println("Button was pressed for the " + pressCount + "th time");
-//            }
-//        });
-//
-//        // OPTIONAL: print the registry
-//        PrintInfo.printRegistry(console, pi4j);
-//
-//        while (pressCount < 5) {
-//            if (led.equals(DigitalState.HIGH)) {
-//                console.println("LED low");
-//                led.low();
-//            } else {
-//                console.println("LED high");
-//                led.high();
-//            }
-//            Thread.sleep(500 / (pressCount + 1));
-//        }
+        // setup an analog input listener to listen for any value changes on the analog input
+        input.addListener(console::println);
 
+        // lets read the analog output state
+        console.print("THE STARTING ANALOG INPUT [" + input + "] VALUE IS [");
+        console.println(input.value() + "]");
 
-        AnalogInput moistureSensor;
-        moistureSensor = pi4j.analogInput().create(PIN_MOISTURE_SENSOR);
-        console.println("VALEUR : " + moistureSensor.getValue());
+        console.println("CHANGE INPUT VALUES VIA I/O HARDWARE AND CHANGE EVENTS WILL BE PRINTED BELOW:");
 
-        // ------------------------------------------------------------
-        // Terminate the Pi4J library
-        // ------------------------------------------------------------
-        // We we are all done and want to exit our application, we must
-        // call the 'shutdown()' function on the Pi4J static helper class.
-        // This will ensure that all I/O instances are properly shutdown,
-        // released by the the system and shutdown in the appropriate
-        // manner. Terminate will also ensure that any background
-        // threads/processes are cleanly shutdown and any used memory
-        // is returned to the system.
+        // wait (block) for user to exit program using CTRL-C
+        console.waitForExit();
 
-        // Shutdown Pi4J
+        // shutdown Pi4J
+        console.println("ATTEMPTING TO SHUTDOWN/TERMINATE THIS PROGRAM");
         pi4j.shutdown();
     }
 }
