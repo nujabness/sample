73353336db4a7bf442d03f4241760bc272b03e61
nujabness <m.elassouri@gmail.com>
2022-01-20 19:15:02 +0100

    Init

--- src/main/java/com/pi4j/example/MinimalExample.java
+++ src/main/java/com/pi4j/example/MinimalExample.java
@@ -106,7 +106,7 @@ public class MinimalExample {
         var valueConfig = DigitalInput.newConfigBuilder(pi4j)
                 .id("button")
                 .name("Press button")
-                .address(7)
+                .address(17)
                 .provider("pigpio-digital-input");
         var value = pi4j.create(valueConfig);
         value.addListener(e -> {
